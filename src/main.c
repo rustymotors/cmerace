3af11cd3968de5602c9fb8d29f029e003efc142d
Molly Draven <drazi@duck.com>
2024-05-27 18:52:22 -0400

    refactor: Add socket server functionality and receive data
    This commit adds functionality to the main.c file to act as a socket server and receive data from a client. It includes changes to create a socket, bind it to a specific port, listen for incoming connections, accept a client connection, receive data from the client, and display the received data. The code also includes error handling for socket creation, binding, listening, accepting connections, and receiving data.

--- src/main.c
+++ src/main.c
@@ -1,25 +1,89 @@
 #include <stdio.h>
 #include <sys/socket.h>
 #include <unistd.h>
-
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
 
 int main(void)
 {
+    void *buff = malloc(1024);
     int r = 0;
     int s;
-    
+    int c;
+    struct sockaddr_in addr = {
+        .sin_family = AF_INET,
+        .sin_port = htons(8226),
+        .sin_addr = {.s_addr = INADDR_ANY}};
+
+    struct sockaddr_in client_addr;
+
     printf("Hello, World!\n");
 
     s = socket(AF_INET, SOCK_STREAM, 0);
 
-    if (s < 0) {
+    if (s < 0)
+    {
         perror("socket");
         return 1;
     }
-    
+
+    if (bind(s, (struct sockaddr *)&addr, sizeof(addr)) < 0)
+    {
+        perror("bind");
+        return 1;
+    }
+
+    r = listen(s, 5);
+
+    if (r < 0)
+    {
+        perror("listen");
+        return 1;
+    }
+
+    c = accept(s, NULL, NULL) ;
+
+    if (c < 0)
+    {
+        printf("Error accepting connection: %d\n", c);
+        perror("accept");
+        return 1;
+    }
+
+    r = getpeername(c, (struct sockaddr *)&client_addr, (socklen_t[]){sizeof(client_addr)});
+
+    if (r < 0)
+    {
+        perror("getpeername");
+        return 1;
+    }
+
+    printf("Connection from %s:%d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+
+    r = recv(c, buff, 1024, 0);
+
+    if (r < 0)
+    {
+        perror("recv");
+        return 1;
+    }
+
+    printf("Received %d bytes\n", r);
+
+    char *incoming = (char *)buff;
+
+    printf("Received: ");
+    for (int i = 0; i < r; i++)
+    {
+        printf("%02x", incoming[i]);
+    }
+    printf("\n");
+
     r = close(s);
 
-    if (r < 0) {
+    if (r < 0)
+    {
         perror("close");
         return 1;
     }
